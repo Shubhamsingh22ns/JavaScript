1d68fe21eff784a268d5397b1890df0deb040aa3
Shubham Singh <shubhamsingh22ns@gmail.com>
2025-04-05 14:33:33 +0530

    comparision in js

---
+++ 04_comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1) // true
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log(null == 0)
+console.log(null >= 0) // 0>=0 => true
+console.log(null > 0) // 0>0 => false
+
+//strict check value + data type
+
+console.log(2 == "2") // true
+console.log(2 === "2") // value + data type = false
