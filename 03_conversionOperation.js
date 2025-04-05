1301331592eabd7428cf0ae49230ccbb96fb4034
Shubham Singh <shubhamsingh22ns@gmail.com>
2025-04-05 12:45:38 +0530

    Conversion Operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,19 @@
+ console.log(4+2)
+ console.log(9-2)
+ console.log(21*2)
+ console.log(9%2)
+ console.log(20/2)
+
+ let str1 = "Shubham"
+ let str2 = " Singh"
+ let str3 = str1+str2
+ console.log(str3)
+
+ //Important
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+console.log(+true); //1
+console.log(+'') //0
