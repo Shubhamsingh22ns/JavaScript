bb64cfa6e585f32126a9b61dcd2ac5070d8a1739
Shubham Singh <shubhamsingh22ns@gmail.com>
2025-04-20 11:33:20 +0530

    Data types summary

---
+++ 05_dataTyoesSummary.js
@@ -0,0 +1,45 @@
+// two types of Data types:
+
+// a) Primitive
+
+// 1. String
+let name = "John Doe";
+// 2. Number
+let age = 30;
+// 3. Boolean
+let isEmployed = true;
+// 4. Undefined
+let job;
+// 5. Null
+let address = null;
+// 6. Symbol
+let uniqueId = Symbol("id");
+let Id = Symbol("id");
+console.log(uniqueId === Id); // false, because symbols are unique
+
+// 7. BigInt
+let bigNumber = BigInt(1234567890123456789012345678901234567890);
+
+// b) Non Primitive/ Refferance
+// 1. Object
+let person = {
+  name: "John Doe",
+  age: 30,
+  isEmployed: true,
+};
+// 2. Array
+let numbers = [1, 2, 3, 4, 5];
+
+// 3. Function
+function greet() {
+  console.log("Hello, World!");
+}
+person.age = 31; // Updating object property
+console.log(person.age); // Accessing object property
+
+console.log(typeof bigNumber); // bigint
+console.log(typeof address); // object
+console.log(typeof name); // string
+console.log(typeof age); // number
+
+
